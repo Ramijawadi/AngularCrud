8286c8fdc2cbf4c4b2f78adc8178430cc88f81fc
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2024-07-29 19:04:03 +0100

    Add Search by firstname functionality

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+// import {Ng2OrderModule} from 'ng2-order-pipe'
+// import {Ng2SearchPipeModule} from 'ng2-search-filter'
+@NgModule({
+  declarations: [
+ 
+   
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,10 +1,15 @@
+import { NgIf ,NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'; // <-- Add this
+import {Ng2OrderModule} from 'ng2-order-pipe'
+import {Ng2SearchPipeModule} from 'ng2-search-filter'
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule , NgFor ,NgIf ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
@@ -12,6 +17,7 @@ export class EmployeeComponent implements OnInit{
 
   http = inject(HttpClient) ;
   employees : any = []
+  firstName : any ;
 
   ngOnInit(): void {
     this.fetchEmployees();
@@ -25,6 +31,29 @@ export class EmployeeComponent implements OnInit{
       console.log(employees);
       this.employees = employees ;
     })
+  }
+
+  Search() {
+
+    if(this.firstName== ""){
+      this.ngOnInit()
+    }
+    else {
+
+      this.employees = this.employees.filter((res: { firstName: string; })=> {
+
+        return res.firstName.toLocaleLowerCase().match(this.firstName.toLocaleLowerCase());
+      })
+
+    }
+  }
+
+key : string = 'id';
+reverse : boolean = false ;
+  sort(key: string){
+
+    this.key = key ;
+    this.reverse = !this.reverse ;
   }
 
 }
