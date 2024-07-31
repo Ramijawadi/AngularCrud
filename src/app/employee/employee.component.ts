af32107c986d33abedb21716b7bff3165aaa9a6b
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2024-07-31 17:43:33 +0100

    FIX : Add Employee run successfully

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -4,7 +4,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule , Validators} from '@angular/forms';
 import { FilterPipe } from '../filter.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { Employee } from './employee.model';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-employee',
@@ -18,22 +19,25 @@ export class EmployeeComponent implements OnInit {
   employees: any = [];
   firstName: any;
   searchText: any;
-
+  employeeObj:Employee = new Employee()
   formValue!: FormGroup;
+  allEmployees : any;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder , private api:ApiService) {}
 
   ngOnInit(): void {
     this.fetchEmployees();
 
     this.formValue = this.formBuilder.group({
-      // id: ['', Validators.required],
+      id: ['', Validators.required],
       age: ['', Validators.required],
+      dob: ['', Validators.required],
       email: ['', Validators.required],
       salary: ['', Validators.required],
-      address: ['', Validators.required],
+      adress: ['', Validators.required],
       lastName: ['', Validators.required],
       firstName: ['', Validators.required],
+      contactNumber: ['', Validators.required],
     });
   }
 
@@ -45,6 +49,63 @@ export class EmployeeComponent implements OnInit {
         this.employees = employees;
       });
   }
+
+add(){
+
+
+}
+
+addEmployee(){
+
+  this.employeeObj.id = this.formValue.value.id;
+  this.employeeObj.age = this.formValue.value.age;
+  this.employeeObj.dob = this.formValue.value.dob;
+
+  this.employeeObj.email = this.formValue.value.email;
+  this.employeeObj.salary = this.formValue.value.salary;
+  this.employeeObj.adress = this.formValue.value.adress;
+  this.employeeObj.imageUrl = this.formValue.value.imageUrl;
+
+  this.employeeObj.firstName = this.formValue.value.firstName;
+  this.employeeObj.lastName = this.formValue.value.lastName;
+  this.employeeObj.contactNumber = this.formValue.value.contactNumber;
+
+
+  this.api.postEmployee(this.employeeObj).subscribe(res=>{
+console.log(res);
+this.formValue.reset();
+this.fetchEmployees()
+    alert('record added successfully !')
+  },
+
+error => {
+
+  alert('something went Wrong !')
+} )
+}
+
+
+//get
+
+// getEmployee(){
+
+//   this.api.getEmployee().subscribe(res=> {
+//  this.allEmployees = res ;
+
+//   })
+// }
+
+//delete
+
+deleteEmployee(data:any) {
+if(confirm('are you sure to delete !'))
+  this.api.deleteEmployee(data.id)
+  .subscribe(res=> {
+    alert('record deleted successfully !');
+    this.fetchEmployees()
+  })
+
+}
 
   Search() {
     if (this.firstName == '') {
--- src/app/employee/employee.model.ts
+++ src/app/employee/employee.model.ts
@@ -1,9 +1,11 @@
 export class Employee {
     id:number = 0;
     age: number=0;
+    dob:string='';
     email:string='';
     salary: number=0;
-    address: string='';
+    adress: string='';
+    imageUrl: string ='';
     lastName: string='';
     firstName: string='';
     contactNumber:number=0;
