cc327c8da9c33c12f1cce3f178cebc954dc7af26
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2025-01-21 10:35:53 +0100

    Fix logo + icons design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { EmployeeComponent } from './employee/employee.component';
-
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
-  declarations: [
-    
-   
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    NgxPaginationModule
-    
+    NgxPaginationModule,
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule {}
